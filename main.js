269753270d7f412e2c7a7e8d8a2a8c3811095e08
Marcos <devler.mve@gmail.com>
2024-09-09 16:44:51 +0200

    fix: main.js has been fixed

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 import './src/lib/assets/styles/style.css'
-import {context} from "./src/core/common/presentation/models/Context.js";
-import {TripsMetricsBloc} from "./src/features/tripsMetrics/presentation/bloc/TripsMetricsBloc.js";
-import {useSetSearchParams} from "./src/core/common/presentation/hooks/useSetSearchParams.js";
-import {useGetSearchParams} from "./src/core/common/presentation/hooks/useGetSearchParams.js";
+import { context } from '@/core/common/presentation/models/Context.js'
+import { TripsMetricsBloc } from '@/features/tripsMetrics/presentation/bloc/TripsMetricsBloc.js'
+import { useSetSearchParams } from '@/core/common/presentation/hooks/useSetSearchParams.js'
+import { useGetSearchParams } from '@/core/common/presentation/hooks/useGetSearchParams.js'
 
-async function app() {
-    const { filterBy } = useGetSearchParams(['filterBy'])
-    context.provide('SelectedDate', filterBy?.day)
+function app() {
+  const { filterBy } = useGetSearchParams(['filterBy'])
+  context.provide('SelectedDate', filterBy?.day)
 
-    const bloc = new TripsMetricsBloc()
-    context.provide('TripsMetricsBloc', bloc)
+  const bloc = new TripsMetricsBloc()
+  context.provide('TripsMetricsBloc', bloc)
 
-    await bloc.dispatch('GetTripsEvent', {day: filterBy?.day})
+  bloc.dispatch('GetTripsEvent', { day: filterBy?.day })
 
-    document.body.addEventListener('onCalendarChange', async (event) => {
-        const {day} = event.detail
-        await bloc.dispatch('GetTripsEvent', {day})
-        useSetSearchParams('filterBy', {day})
-    });
-
+  document.body.addEventListener('onCalendarChange', async (event) => {
+    const { day } = event.detail
+    await bloc.dispatch('GetTripsEvent', { day })
+    useSetSearchParams('filterBy', { day })
+  })
 }
 
-await app()
+app()
